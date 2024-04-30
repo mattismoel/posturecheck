8020020d0c80f58fe637048168b90cb9fa0cb2bf
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-04-30 19:45:33 +0200

    add documentation

--- main.go
+++ main.go
@@ -8,17 +8,23 @@ import (
 	"net/http"
 )
 
+// Embedding af templates: HTML-filer i "template"-mappen.
+//
 //go:embed template
 var tmplFS embed.FS
 
+// Embedding af statiske filer: CSS-filer i "static"-mappen.
+//
 //go:embed static
 var staticFS embed.FS
 
+// Antal gange at en "backpain" er registreret siden server start.
 var checkCount = 0
 
 func main() {
 	mux := http.NewServeMux()
 
+	// Gør statiske filer tilgængelige på "localhost:8080/static/...".
 	staticFS, err := fs.Sub(staticFS, "static")
 	if err != nil {
 		log.Fatal(err)
@@ -26,23 +32,29 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticFS))))
 
+	// Registrering af stier på webserveren.
+	mux.HandleFunc("/", handleIndex())
 	mux.HandleFunc("POST /add", handleAdd())
 	mux.Handle("GET /count", handleGetCount())
-	mux.HandleFunc("/", handleIndex())
 
 	log.Println("serving on :8080")
+
+	// Opstart af serveren.
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
+// Håndterer registrering af backpains ved POST request.
 func handleAdd() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		checkCount++
-		w.Write([]byte("Added one"))
 	}
 }
 
+// Håndterer rendering af startsiden.
 func handleIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Gennemtjek nødvendige filer.
+		// Hvis der er syntaksfejl eller lignende, skriver vi fejl til browseren.
 		tmpl, err := template.ParseFS(
 			tmplFS,
 			"template/index.html",
@@ -53,11 +65,14 @@ func handleIndex() http.HandlerFunc {
 			return
 		}
 
+		// Opsætnign af nødvendig data-map til vores startside.
 		data := map[string]any{
 			"checkCount": checkCount,
 			"message":    "Some message...",
 		}
 
+		// Skriv respons til ResponseWriter (w), med ovenstående data.
+		// Hvis fejl opstår, skriver vi fejl til browseren.
 		err = tmpl.Execute(w, data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -66,19 +81,23 @@ func handleIndex() http.HandlerFunc {
 	}
 }
 
+// Håndtering af GET request af ny counter.
 func handleGetCount() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Gennemtjek counter.html for eventuelle syntaksfejl.
 		tmpl, err := template.ParseFS(tmplFS, "template/counter.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// Opsætning af nødvendig data for counter.html.
 		data := map[string]any{
 			"checkCount": checkCount,
 			"message":    "Some other message...",
 		}
 
+		// Forsøg at skriv respons til request med den definerede data.
 		err = tmpl.Execute(w, data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
--- main.py
+++ main.py
@@ -2,36 +2,52 @@ from sense_hat import SenseHat
 import time
 import requests
 
-s = SenseHat()
-
+# Størrelsen af LED-matrixen på den fysiske sense hat.
 LED_MATRIX_SIZE = 8
+
+# Farven som sense hat blinker, når backpain registreres.
+BLINK_COLOR = [255,0,0]
+
+# sense hat's rotation. benytter sig udelukkende af dens 'pitch' del.
 rotation = 0.0
+
+# Den maksimale vinkel, før at et backpain kan registereres.
 max_angle = 30.0
 
-time_before_signal = 5.0
+# Mængde af tid, at brugeren skal være over 'max_angle' før at et backpain
+# registreres.
+registration_duration = 5.0
+
+# Mængde af tid siden brugeren overskred 'max_angle'. Denne nulstilles når
+# brugeren igen er i ordenlig holdning.
 time_since_overshoot = 0.0
 
-blink_color = [255,0,0]
+# Tidsforkellen mellem to frames. Benyttes til korrekt udregning af
+# tidsændringer.
 delta = 0.0
 
-isDown = False
+# Er brugeren over 'max_angle'?
+is_user_down = False
 
+# Opsætning af sensehat-variablen 's'.
+s = SenseHat()
 s.clear()
 
+# Definerer hvordan at en input sensehat skal blinke.
+# Hvert "cell" i vores LED-matrix sættes til vores 'blink_color'-variabel.
 def blink(sensehat):
-    pixels = []
-    for i in range(LED_MATRIX_SIZE):
-        for j in range(LED_MATRIX_SIZE):
-            pixels.append(blink_color)
-    sensehat.set_pixels(pixels)
+    cells = []
+    for _ in range(LED_MATRIX_SIZE):
+        for _ in range(LED_MATRIX_SIZE):
+            cells.append(BLINK_COLOR)
+
+    sensehat.set_pixels(cells)
 
 
+# Definerer funktionalitet for håndtering af backpain-registrering.
+# Et POST request sendes til web-serveren.
 def backpain():
-    global isDown
-    isDown = True
-
-    res = requests.post("http://localhost:8080/add")
-    print(res.content)
+    requests.post("http://localhost:8080/add")
     blink(s)
 
 
@@ -39,17 +55,24 @@ while True:
     t = time.time()
 
     rotation = s.get_orientation_degrees()["pitch"] - 270
-    if isDown:
+    # Hvis brugeren har dårlig holdning, men retter sig op, sidder er brugeren
+    # ikke længere nede.
+    if is_user_down:
         if rotation <= max_angle:
-            isDown = False
+            is_user_down = False
+
+    # Hvis brugeren ikke er nede, og brugeren overskrider den maksimale vinkel
+    # starter vi med at tælle med 'time_since_overshoot'. Hvis
+    # 'time_since_overshoot' overskrider vores 'registration_duration',
+    # registrerer vi backpain.
     else:
-        if rotation >= max_angle and not isDown:
+        if rotation >= max_angle:
             time_since_overshoot += delta
 
-        if time_since_overshoot >= time_before_signal:
+        if time_since_overshoot >= registration_duration:
             time_since_overshoot = 0.0
+            is_user_down = True
             backpain()
 
+    # Udregning af delta - benyttes til at tælle 'time_since_overshoot'.
     delta = time.time() - t
-
-
