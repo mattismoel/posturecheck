eff0281397399ef9831aee097d8a20fdefb0cb22
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-04-30 19:18:06 +0200

    update count implementation

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -10,12 +11,23 @@ import (
 //go:embed template
 var tmplFS embed.FS
 
-var CheckCount = 0
+//go:embed static
+var staticFS embed.FS
+
+var checkCount = 0
 
 func main() {
 	mux := http.NewServeMux()
 
+	staticFS, err := fs.Sub(staticFS, "static")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticFS))))
+
 	mux.HandleFunc("POST /add", handleAdd())
+	mux.Handle("GET /count", handleGetCount())
 	mux.HandleFunc("/", handleIndex())
 
 	log.Println("serving on :8080")
@@ -24,20 +36,50 @@ func main() {
 
 func handleAdd() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		CheckCount++
+		checkCount++
 		w.Write([]byte("Added one"))
 	}
 }
 
 func handleIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFS(tmplFS, "template/index.html")
+		tmpl, err := template.ParseFS(
+			tmplFS,
+			"template/index.html",
+			"template/counter.html",
+		)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		err = tmpl.Execute(w, CheckCount)
+		data := map[string]any{
+			"checkCount": checkCount,
+			"message":    "Some message...",
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func handleGetCount() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFS(tmplFS, "template/counter.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := map[string]any{
+			"checkCount": checkCount,
+			"message":    "Some other message...",
+		}
+
+		err = tmpl.Execute(w, data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
