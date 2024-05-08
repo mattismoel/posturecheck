b333f25cbd3060e7425499512a1a83b6b5a4018d
Mattis Møl Kristensen <mattismoel@gmail.com>
2024-05-08 21:09:20 +0200

    refactor handlers to separate file

--- main.go
+++ main.go
@@ -4,14 +4,11 @@ import (
 	"embed"
 	"errors"
 	"fmt"
-	"html/template"
 	"io/fs"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 // Embedding af templates: HTML-filer i "template"-mappen.
@@ -58,82 +55,6 @@ func main() {
 
 	// Opstart af serveren.
 	log.Fatal(http.ListenAndServe(":8080", mux))
-}
-
-// Håndterer registrering af backpains ved POST request.
-func handleAdd() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		checkCount++
-	}
-}
-
-// Håndterer rendering af startsiden.
-func handleIndex() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		cookieCount, err := cookieCount(r)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		checkCount = cookieCount
-
-		// Gennemtjek nødvendige filer.
-		// Hvis der er syntaksfejl eller lignende, skriver vi fejl til browseren.
-		tmpl, err := template.ParseFS(
-			tmplFS,
-			"template/index.html",
-		)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		msg := countToMsg(checkCount)
-
-		// Opsætnign af nødvendig data-map til vores startside.
-		data := map[string]any{
-			"checkCount": checkCount,
-			"message":    msg,
-		}
-
-		// Skriv respons til ResponseWriter (w), med ovenstående data.
-		// Hvis fejl opstår, skriver vi fejl til browseren.
-		err = tmpl.Execute(w, data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
-}
-
-// Håndtering af GET request af ny counter.
-func handleGetCount() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		setCookieCount(w, checkCount)
-
-		// Gennemtjek counter.html for eventuelle syntaksfejl.
-		tmpl, err := template.ParseFS(tmplFS, "template/index.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		msg := countToMsg(checkCount)
-
-		// Opsætning af nødvendig data for counter.html.
-		data := map[string]any{
-			"checkCount": checkCount,
-			"message":    msg,
-		}
-
-		// Forsøg at skriv respons til request med den definerede data.
-		err = tmpl.ExecuteTemplate(w, "counter", data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
 }
 
 func countToMsg(count int) string {
